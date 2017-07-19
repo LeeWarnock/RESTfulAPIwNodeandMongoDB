e4a6576f8b21e3cd5b6bd8883679dcbfde07ed18
Lee Warnock <clwarnock@gmail.com>
2017-07-19 13:28:22 -0500

    add users code

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 var express     =   require("express");
 var app         =   express();
 var bodyParser  =   require("body-parser");
+var mongoOp     =   require("./model/mongo")
 var router      =   express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
-router.get("/",function(req,res){
-    res.json({"error" : false,"message" : "Hello World! Router has connected you to the server."});
-});
+router.route("/users")
+    .get(function(req,res){
+        var response = {};
+        mongoOp.find({},function(err,data){
+        // Mongo command to fetch all data from collection.
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+            res.json(response);
+        });
+    });
 
 app.use('/',router);
 
 app.listen(3000);
-console.log("Listening to PORT 3000");
+console.log("Listening on PORT 3000");
