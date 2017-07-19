749b8e770e126253bb92f2e44ef08a400fbddabd
Lee Warnock <clwarnock@gmail.com>
2017-07-19 12:19:24 -0500

    add style to server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
 router.get("/",function(req,res){
-    res.json({"error" : false,"message" : "Hello World"});
+    res.json({"error" : false,"message" : "Hello World! Router has connected you to the server."});
 });
 
 app.use('/',router);
