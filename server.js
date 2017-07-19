d564a973e42516730766d8109909d9494305e20f
Lee Warnock <clwarnock@gmail.com>
2017-07-19 13:31:34 -0500

    post users

--- server.js
+++ server.js
@@ -7,15 +7,36 @@ var router      =   express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
+router.get("/",function(req,res){
+    res.json({"error" : false,"message" : "Hello World"});
+});
+
+//route() will allow you to use same path for different HTTP operation.
+//So if you have same URL but with different HTTP OP such as POST,GET etc
+//Then use route() to remove redundant code.
+
 router.route("/users")
     .get(function(req,res){
+        ------------------------------------------------------
+    })
+    .post(function(req,res){
+        var db = new mongoOp();
         var response = {};
-        mongoOp.find({},function(err,data){
-        // Mongo command to fetch all data from collection.
+        // fetch email and password from REST request.
+        // Add strict validation when you use this in Production.
+        db.userEmail = req.body.email;
+        // Hash the password using SHA1 algorithm.
+        db.userPassword =  require('crypto')
+                          .createHash('sha1')
+                          .update(req.body.password)
+                          .digest('base64');
+        db.save(function(err){
+        // save() will run insert() command of MongoDB.
+        // it will add new data in collection.
             if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
+                response = {"error" : true,"message" : "Error adding data"};
             } else {
-                response = {"error" : false,"message" : data};
+                response = {"error" : false,"message" : "Data added"};
             }
             res.json(response);
         });
@@ -24,4 +45,4 @@ router.route("/users")
 app.use('/',router);
 
 app.listen(3000);
-console.log("Listening on PORT 3000");
+console.log("Listening to PORT 3000");
